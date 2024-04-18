ab8e4ab0a5ea089100939978b320980986cb5408
conner <conner.tran@bsa.edu.com>
2024-04-18 14:22:45 +0200

    fixingdb.js and completing the models

--- db.js
+++ db.js
@@ -2,7 +2,9 @@
 
 const pg = require("pg");
 
-const db = new pg.Client("postgresql:///lunchly");
+const db = new pg.Client(
+  "postgresql://postgres:postgres@localhost:5432/lunchly"
+);
 
 db.connect();
 
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 /** Start server for Lunchly. */
+// command for start : nodemon -e js,html,css server.js
 
 const app = require("./app");
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("listening on 3000");
 });
